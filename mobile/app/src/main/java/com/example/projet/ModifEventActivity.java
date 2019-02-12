cf2d32d765d75235154109e03a5439ba4b06114b
LioClem <e187485n@ubuntu-info>
2019-02-12 13:24:24 +0100

    layout_event

---
+++ mobile/app/src/main/java/com/example/projet/ModifEventActivity.java
@@ -0,0 +1,15 @@
+package com.example.projet;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ModifEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modifevent);
+    }
+
+
+}
