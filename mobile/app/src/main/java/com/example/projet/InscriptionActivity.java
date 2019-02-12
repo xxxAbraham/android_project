ddeab025fa7920c999b8a6777553346ed4373647
xxxAbraham <abraham2255@hotmail.fr>
2019-02-12 15:01:12 +0100

    Modification de LoginActivity

--- mobile/app/src/main/java/com/example/projet/InscriptionActivity.java
+++ mobile/app/src/main/java/com/example/projet/InscriptionActivity.java
@@ -1,33 +1,15 @@
 package com.example.projet;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
+
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
 
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
-
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.text.TextUtils;
-import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
@@ -38,20 +20,11 @@ import com.google.gson.JsonObject;
 import com.koushikdutta.async.future.FutureCallback;
 import com.koushikdutta.ion.Ion;
 
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-
-
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 
 /**
- * A login screen that offers login via email/password.
+ @author Abraham Gabsi
  */
 public class InscriptionActivity extends AppCompatActivity {
     // UI references.
@@ -154,8 +127,6 @@ public class InscriptionActivity extends AppCompatActivity {
     private boolean isPasswordValid(String password) {
         return password.length() > 4;
     }
-
-
 
 }
 
