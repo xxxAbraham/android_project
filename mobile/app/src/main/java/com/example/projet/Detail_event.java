c80e4a0cc6a41fd0ff3a3ee5117ce0458ae0f2de
mdiya <thomas.pigot@gmail.com>
2019-02-12 18:49:06 +0100

    MaJ layout

---
+++ mobile/app/src/main/java/com/example/projet/Detail_event.java
@@ -0,0 +1,13 @@
+package com.example.projet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Detail_event extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_event);
+    }
+}
