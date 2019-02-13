0b3c8632039345296077270cafc613ef6d6a38cd
mdiya <thomas.pigot@gmail.com>
2019-02-13 13:23:49 +0100

    MaJ layout

---
+++ mobile/app/src/main/java/com/example/projet/details_pseudo_event.java
@@ -0,0 +1,13 @@
+package com.example.projet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class details_pseudo_event extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details_pseudo_event);
+    }
+}
