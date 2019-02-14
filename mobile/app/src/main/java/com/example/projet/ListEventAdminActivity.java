bf1c1e63f5814a02908c4d0c9672591e1d404c27
e1603386 <allain.e1603386@etud.univ-ubs.fr>
2019-02-14 11:46:27 +0100

    commmit sympa

--- mobile/app/src/main/java/com/example/projet/ListEventAdminActivity.java
+++ mobile/app/src/main/java/com/example/projet/ListEventAdminActivity.java
@@ -2,6 +2,8 @@ package com.example.projet;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 public class ListEventAdminActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class ListEventAdminActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listeventadmin);
+
+        TextView name = (TextView) findViewById(R.id.txt_name);
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,R.layout.activity_listeventadmin,R.id.name,StringArray);
+
+
+
     }
 }
