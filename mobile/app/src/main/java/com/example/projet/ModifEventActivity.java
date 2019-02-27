2166190642b9f3dac88b7b333631f5c3b3303f2a
mdiya <thomas.pigot@gmail.com>
2019-02-27 18:55:53 +0100

    MaJ layout

--- mobile/app/src/main/java/com/example/projet/ModifEventActivity.java
+++ mobile/app/src/main/java/com/example/projet/ModifEventActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
@@ -76,34 +77,61 @@ public class ModifEventActivity extends AppCompatActivity implements DatePickerD
                         title.setText(nom);
                         date.setText(result.get("date").getAsString());
                         place.setText(result.get("place").getAsString());
+                        desc.setText(result.get("description").getAsString());
 
                     }});
 
         valider.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String urlmodif = "http://10.0.2.2:8080/api/evenement/update/"+eventid;
-                final JsonObject json = new JsonObject();
-                json.addProperty("userId", prefs.getString("id", ""));
-                json.addProperty("title", title.getText().toString());
-                json.addProperty("date", date.getText().toString());
-                json.addProperty("place", place.getText().toString());
-                json.addProperty("description", desc.getText().toString());
-                Ion.with(ModifEventActivity.this)
-                        .load("PUT",urlmodif)
-                        .setJsonObjectBody(json)
-                        .asJsonObject()
-                        .setCallback(new FutureCallback<JsonObject>() {
-                            @Override
-                            public void onCompleted(Exception e, JsonObject result) {
-                                if (result.get("ok").getAsBoolean()) {
-                                    finish();
-                                } else {
-                                    Toast.makeText(ModifEventActivity.this, "Champs incorecte",
-                                            Toast.LENGTH_LONG).show();
+                View focusView = null;
+                boolean cancel = false;
+                if (TextUtils.isEmpty(title.getText())) {
+                    title.setError("Entrez un nom!");
+                    focusView = title;
+                    cancel = true;
+                }
+                if (TextUtils.isEmpty(date.getText())) {
+                    date.setError("Entrez une date (yyyy-mm-dd)!");
+                    focusView = date;
+                    cancel = true;
+                }
+                if (TextUtils.isEmpty(place.getText())) {
+                    place.setError("Entrez une adresse!");
+                    focusView = place;
+                    cancel = true;
+                }
+                if (TextUtils.isEmpty(desc.getText())) {
+                    desc.setError("Entrez une description!");
+                    focusView = desc;
+                    cancel = true;
+                }
+                if (cancel) {
+                    focusView.requestFocus();
+                } else {
+                    String urlmodif = "http://10.0.2.2:8080/api/evenement/update/" + eventid;
+                    final JsonObject json = new JsonObject();
+                    json.addProperty("userId", prefs.getString("id", ""));
+                    json.addProperty("title", title.getText().toString());
+                    json.addProperty("date", date.getText().toString());
+                    json.addProperty("place", place.getText().toString());
+                    json.addProperty("description", desc.getText().toString());
+                    Ion.with(ModifEventActivity.this)
+                            .load("PUT", urlmodif)
+                            .setJsonObjectBody(json)
+                            .asJsonObject()
+                            .setCallback(new FutureCallback<JsonObject>() {
+                                @Override
+                                public void onCompleted(Exception e, JsonObject result) {
+                                    if (result.get("ok").getAsBoolean()) {
+                                        finish();
+                                    } else {
+                                        Toast.makeText(ModifEventActivity.this, "Champs incorecte",
+                                                Toast.LENGTH_LONG).show();
+                                    }
                                 }
-                            }
-                        });
+                            });
+                }
             }
         });
     }
