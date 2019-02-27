b58eadc21722debc7a920cd374b12f4a3179d68d
LioClem <e187485n@ubuntu-info>
2019-02-27 17:21:54 +0100

    date modif event

--- mobile/app/src/main/java/com/example/projet/ModifEventActivity.java
+++ mobile/app/src/main/java/com/example/projet/ModifEventActivity.java
@@ -1,11 +1,15 @@
 package com.example.projet;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,14 +19,20 @@ import com.google.gson.JsonObject;
 import com.koushikdutta.async.future.FutureCallback;
 import com.koushikdutta.ion.Ion;
 
+import java.util.Calendar;
 import java.util.Iterator;
 
-public class ModifEventActivity extends AppCompatActivity {
+public class ModifEventActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
 
     public static final String MY_PREFS_NAME = "MyPrefsFile";
 
     String eventid = "";
     TextView title, date, place, desc;
+    Calendar d = Calendar.getInstance();
+    String year;
+    String month;
+    String day;
+    DatePickerDialog datePickerDialog;
     Button valider;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,11 +44,21 @@ public class ModifEventActivity extends AppCompatActivity {
         desc = findViewById(R.id.txt_descriptionModif);
         valider = findViewById(R.id.btnFinalModif);
         final SharedPreferences prefs = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE);
-
+        datePickerDialog = new DatePickerDialog(
+                ModifEventActivity.this, ModifEventActivity.this, d.get(Calendar.YEAR), d.get(Calendar.MONTH), d.get(Calendar.DAY_OF_MONTH));
         final Intent intent = getIntent();
         if(intent.hasExtra("eventid")){
             eventid = intent.getStringExtra("eventid");
         }
+
+        date.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                datePickerDialog.show();
+                datePickerDialog.getButton(DatePickerDialog.BUTTON_NEGATIVE).setText("Fermer");
+                return true;
+            }
+        });
 
         String url = "http://10.0.2.2:8080/api/evenement/get/"+eventid;
         Ion.with(ModifEventActivity.this)
@@ -89,4 +109,26 @@ public class ModifEventActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+        Log.i("ON DATA SET", "onDateSet: "+ i + " *** "+ i1 + " *** "+ i2);
+        i1 ++;
+        if (i1 < 10){
+            month = "0"+i1;
+        } else {
+            month = ""+i1;
+        }
+
+        if(i2<10){
+            day = "0"+i2;
+        } else {
+            day = ""+i2;
+        }
+
+        year = ""+i;
+
+
+        Log.i("ON DATA SET", "onDateSet: "+ year + " *** "+ month + " *** "+ day);
+        date.setText(year+"-"+month+"-"+day);
+    }
 }
